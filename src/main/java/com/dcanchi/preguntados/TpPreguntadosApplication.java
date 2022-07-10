eef6d4f37fdf5db79b7ac0e57cf187fc5bd6c4d3
DavidCanchi <davidcanchi@gmail.com>
2022-07-10 15:36:51 -0300

    subo proyecto inicial

---
+++ src/main/java/com/dcanchi/preguntados/TpPreguntadosApplication.java
@@ -0,0 +1,13 @@
+package com.dcanchi.preguntados;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TpPreguntadosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TpPreguntadosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dcanchi/preguntados/TpPreguntadosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dcanchi.preguntados;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TpPreguntadosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
